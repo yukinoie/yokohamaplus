2c3111bd44248e7e355410c2f4b3a870ae3389d8
yukinoie <107175398+yukinoie@users.noreply.github.com>
2022-12-08 15:02:08 +0900

    Add files via upload

---
+++ index_1207.php
@@ -0,0 +1,474 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!---サイトのタイトル--->
+  <title>YokohamaPlus</title>
+
+  <!---google fonts読み込み--->
+  <link href="https://fonts.googleapis.com/css2?family=Overpass:wght@100&display=swap" rel="stylesheet">
+
+  <!---css読み込み--->
+  <link rel="stylesheet" href="./css/reset.css">
+  <link rel="stylesheet" href="./css/style_1207.css">
+</head>
+
+
+<body>
+  <header>
+
+  <!------ヘッダ------->
+  <div class="header" id="g-nav">
+      <!------ロゴ------->
+      <h2 class="sitelogo"><img src="./img/header/logo.png" alt="Yokohamaplusロゴ" class="logo"></h2>
+
+      <!------レスポンシブ ハンガーバーメニューresponsive------->
+      <div id="hamburger">
+          <div></div>
+          <div></div>
+          <div></div>
+      </div>
+
+
+
+      <!------レスポンシブ ハンガーバーメニューリストresponsive------->
+      <nav>
+        <ul id="nav" class="globalnav-list">
+          <li><a href="#top">Top</a></li>
+          <li><a href="#profile">Profile</a></li>
+          <li><a href="#lesson">オンラインヨガ</a></li>
+          <li><a href="#lesson">ベーシックヨガ</a></li>
+          <li><a href="#lesson">女子ダイエット部</a></li>
+          <li><a href="#reserve&contact">予約&お問い合わせ</a></li>
+          <li><a href="#access">アクセス</a></li>
+         </ul>
+       </nav>
+      </div>
+  </header>
+
+
+
+
+
+      
+ <!------メインビジュアル勉強会踏襲------->
+
+ <div class="mainvisual-2" id="top-2">
+   <div class="box-1"></div>
+
+   <div class="layout1" style="position: relative; Z-index: 2;">
+    <div>
+      <img src="./img/main/mainvisual_icon.svg" alt="横浜の京急線井土ヶ谷駅から徒歩7分!" class="icon_header">
+    </div>
+
+
+    <h2 class="sp-view title1">横浜の予約制 少人数ヨガ&ウォーキング教室</h2>
+
+    <div class="sp-view spm-title">
+     <div class="sp-view layout2">
+      <div>
+        <img src="./img/main/500_yen.svg" class="sp-view icon_500yen" alt="500円">
+      </div>
+      <div>
+        <h3 class="title2">体験レッスン<span class="sp-view"><br></span>受付中</h3>
+      </div>
+    </div>
+ 
+    <div id="sp-view text1-posion">
+      <p class="sp-view text1">
+      人の数だけ体格や動作は異なります。<br>
+      Yokohama Plusでのレッスンでは、<br>
+      お一人おひとりに寄り添って改善箇所を見つけ、<br>
+      一5才の体型と体力の変化を念頭に<br>
+      スタイル確立まで導きます。<br>
+      レッスンでの知識はあなたの一生の宝物に<br>なるはずです。
+      </p>
+   </div>
+
+    <div class="yourself-icon sp-view">
+      <img src="./img/main/design yourselfPC.png" alt="design yourself">
+    </div>
+
+    <div class="btn-layout">
+      <a class="moushikomi_button2" href="#reserve&contact" alt="体験レッスンに申し込む">体験レッスンに<br>申し込む</a>
+    </div>
+   </div>
+  </div>
+ </div>
+
+
+
+
+
+
+
+
+ <!------メインビジュアル------->
+  <div class="mainvisual" id="top">
+    <div class="box-1"></div>
+
+    <div class="mainvisual-inner">
+      <!------10/28 Div mainvisual-inner追加変更分------->
+      <img src="./img/main/mainvisual_icon.svg" alt="横浜の京急線井土ヶ谷駅から徒歩7分!" class="icon_header">
+      <img src="./img/main/design yourselfPC.png" alt="design yourself" class="yourself_header">
+      <img src="./img/main/500yen.svg" alt="500円" class="icon_500yen">
+      <h2 class="main_subtitle">横浜の予約制 少人数ヨガ&ウォーキング教室</h2>
+      <h3 class="main_maintitle">体験レッスン受付中</h3>
+
+      <p class="main_text">
+        人の数だけ体格や動作は異なります。<br>
+        Yokohama Plusでのレッスンでは、<br>
+        お一人おひとりに寄り添って改善箇所を見つけ、<br>
+        一5才の体型と体力の変化を念頭に<br>
+        スタイル確立まで導きます。<br>
+        レッスンでの知識はあなたの一生の宝物に<br>なるはずです。
+      </p>
+
+
+      <div>
+        <a class="moushikomi_button" href="#reserve&contact" alt="体験レッスンに申し込む">体験レッスンに<br>申し込む</a>
+      </div>
+    </div>
+  </div>
+
+  <!------メイン2------->
+
+  <div class="container2">
+
+    <div class="mainvisual-can">
+
+      <!------h２タイトル------->
+      <h2 class="yokohamaplus">
+        <!------h２タイトル文言------->
+        <img src="./img/main/title_yokohamaplus.png" alt="yokohamaplusで出来る事">
+        <!------ストライプ------->
+        <div class="title_back_stripe"></div>
+      </h2>
+
+
+      <!------チェックポイント------->
+      <section class="checkpoint">
+      <div class="checkpoint_inner">
+
+        <div class="list-1">
+          <ul>
+            <li>出産後、お腹がぽっこり。体重も戻らない。自分一人だとダイエットが難しい。。</li>
+            <li>在宅ワークや車移動で運動不足が続いているが、ジムに定期的に通う環境と時間がない。</li>
+            <li>オンラインも組み合わせてレッスンがしたい。ヨガの世界観についても興味がある。</li>
+            <li>独学での柔軟、基礎体力作りだと、効果効率が分からない。自分だけのメニューがあれば効率がいいのに。。</li>
+            <li>若い頃より痩せづらく、疲れやすくなった気がする。肩が凝る事や腰が痛くなる事も多いので改善したい。</li>
+          </ul>
+        </div>
+
+        <div class="sp-view triangle"></div>
+
+        <div class="circle-1">
+              <img src="./img/main/kore_maru.png" alt="これ!解決出来ます。" class="circle-kore">
+        </div>
+
+      </div>
+      </section>
+
+
+    </div>
+
+
+      <!------４つの理由------->
+      <section class="reason-4-all">
+        <div class="reason4-titleback"> </div>
+        <h2 class="reason-4">
+          <img src="./img/main/title_4reason.png" alt="4つの理由">
+          <!--------ストライプ----------->
+          <div class="title_back_stripe-2"></div>
+        </h2>
+
+        <div class="reason-4-categly">
+           <div><img src="./img/main/reason2.png" alt="どうなりたいかのゴールを一緒に決める個人の為のサポートがある" class="reason1"></div>
+           <div><img src="./img/main/reason1.png" alt="時間が無くともオンラインを含めた家でも出来る効果効率の良いヨガが出来る" class="reason1"></div>
+           <div><img src="./img/main/reason3.png" alt="ヨガだけではなく、姿勢、栄養面から足りない部分を補強出来る総合したアドバイスが貰える" class="reason1"></div>
+           <div><img src="./img/main/reason4.png" alt="継続して体型、体力をキープ出来るように無理のないレッスンが手軽に受講出来る" class="reason1"></div>
+         </div>
+
+
+
+      </section>
+
+      <!------プロフィール------->
+
+      <section class="profile" id="profile">
+
+        <h2 class="profile_title">
+          <img src="./img/main/title-Profile.png" alt="プロフィール">
+          <!--------ストライプ----------->
+          <div class="title_back_stripe-3"></div>
+        </h2>
+
+        <div class="profile_main">
+
+          <div class="profile_main_inner">
+
+            <!------プロフィール写真はorder1------->
+            <figure>
+              <img src="./img/main/kobayashi_ph.png" alt="小林亜希"  class="profile_photo order1">
+            </figure>
+
+            <!------プロフィール経歴はorder3------->
+            <div id="profile_text_all_order3">
+              <h2 class="sec-title">経歴</h2><br>
+
+              <ul class="keireki">
+                <li>Yokohama Plus主宰<br></li>
+                <li>米国ヨガアライアンス RYT200修了<br></li>
+                <li>YCアシュタンガヨガプライマリーシリーズ修了<br></li>
+                <li>日本ウオーキングインストラクター協会認定インストラクター<br></li>
+                <li>MACSエアロビクス養成コース修了<br></li>
+                <li>都内、神奈川県内のフィットネスクラブにてエアロビクスクラス、筋コンディショニングクラスを担当<br></li>
+                <li>アンダーザライト<br></li>
+                <li>管理栄養士<br></li>
+                <li>ハーブコーディネーター養成講座修了ハーブ料理の会開催<br></li>
+                <li>CAチアリーディングコーチ<br></li>
+                <li>全日本選抜チアリーディング選手権大会準優勝<br></li>
+              </ul>
+
+            </div>
+
+            <!------プロフィールテキストはorder２------->
+           <div class="profile_textorder2">
+            <p>
+            初めまして、Yokohama Plus代表の小林亜希です。横浜の京急線井土ヶ谷駅から徒歩7分という立地でヨガとウォーキングのインストラクターとして活動しております。また、その一方で、管理栄養士としての仕事も請け負っております。<br>
+            この3つスキルを組み合わせたトータルな目線で、総合的な健康美を目指す方へのサポートを得意としております。<br>
+            ここで覚えることは、美しい体型と健康を維持するだけではなく改めて自分の内面とも向き合うという生涯を通しての財産になります。ぜひ体験ご参加をお待ちしております。
+             </p>
+             <div class="kobayashi_sign">
+              <img src="./img/main/kobayashi-sign.png" alt="小林亜希">
+           </div>
+          </div>
+          </div>
+
+  
+        </div>
+
+      </section>
+
+      <!------レッスン内容------->
+
+      <h2 class="lessontitle" id="lesson">
+        <img src="./img/main/lesson.svg" alt="レッスン内容">
+        <!--------ストライプ----------->
+        <div class="title_back_stripe-2"></div>
+      </h2>
+
+      <!------レッスン内容------->
+      <div class="lesson3part_img">
+        <a href="./smallpage_yoga.html" alt="オンラインヨガ" class="icon-button1"><img src="./img/main/online_icon.jpg"></a>
+        <a href="./smallpage_basicyoga.html" alt="ウォーキングヨガ" class="icon-button2"><img src="./img/main/walking_icon.jpg"></a>
+        <a href="./smallpage_diet.html" alt="ダイエット" class="icon-button3"><img src="./img/main/diet_icon.jpg"></a>
+      </div>
+
+  </div>
+
+
+
+  <div class="container3">
+      <a class="HP_button" href="https://yoga-yokohama.com" alt="HPを見る">Yokohama PlusのHPを見る</a>
+      <a class="moushikomi_button3" href="#reserve&contact" alt="体験レッスンに申し込む">体験レッスンに<br>申し込む</a>
+    </div>
+
+
+    <div class="container4">
+      <div class="width1000px">
+       <h2 class="okyakusamanokoe_title" alt="お客様の声">お客様の声</h2>
+
+
+       <!------カードレイアウトをラッピング------->
+       <section class="kuchikomi">
+
+        <div class="kuchikomi_card">
+          <figure class="kao_img"><img src="./img/main/Y.jpg" alt="お客様1"></figure>
+          <h3 class="kuchikomi_name">Yさん<br>
+           オンラインヨガクラス受講</h3>
+          <p class="kuchikomi_text">ヨガ初体験で不安でしたが親切に優しく教えてくださりとても心地良い時間を過ごしています。<br>
+           充実した1時間のレッスンです。<br>
+           ヨガは日々取り入れられる教えがあるのですね。姿勢を正し、心も身体もゆったりと余裕をもって過ごせるようになりたいです。</p>
+        </div>
+
+
+        <div class="kuchikomi_card">
+          <figure class="kao_img"><img src="./img/main/E.jpg" alt="お客様2"></figure>
+           <h3 class="kuchikomi_name">Eさん<br>
+           ベーシックヨガクラス受講</h3>
+           <p class="kuchikomi_text">レッスン翌日、同僚に『やせた？』と言われました。<br>
+            自分の歩きをここまで意識したことはなく、本当に参加してよかった。これからは綺麗な歩きを楽しみます。<br>
+            変わった姿にびっくり!たかがウォーキングではない。また習いたい。</p>
+        </div>
+
+        <div class="kuchikomi_card">
+           <figure class="kao_img"><img src="./img/main/K.jpg" alt="お客様3"></figure>
+           <h3 class="kuchikomi_name">Kさん<br>
+           オンラインヨガクラス受講</h3>
+           <p class="kuchikomi_text">ありがとうございました。<br>
+            久々のたっぷりヨガを噛みしめながら参加しました。体のあちこちが伸びていくのを感じ、呼吸でリラックスをし、満喫させていただきました。<br>
+            楽しかったです!</p>
+        </div>
+
+        <div class="kuchikomi_card">
+           <figure class="kao_img"><img src="./img/main/S.jpg" alt="お客様4"></figure>
+           <h3 class="kuchikomi_name">Sさん<br>
+           ベーシックヨガクラス受講</h3>
+           <p class="kuchikomi_text">自分の姿にショックを受けましたが、歩くだけでいつもと違う筋肉を使ったのかすごく疲れました。<br>
+            癖を細かく直してもらって、体はもちろん気持ち的にも意識しないと!と思えました。もっと年をとってからの癖の積み重ねが本当に怖いので今のうちに直したいです。</p>
+        </div>
+
+
+        <div class="kuchikomi_card">
+           <figure class="kao_img"><img src="./img/main/H.jpg" alt="お客様5"></figure>
+           <h3 class="kuchikomi_name">Hさん<br>
+           オンラインヨガクラス受講</h3>
+           <p class="kuchikomi_text">ヨガだけでなく、筋トレも取り入れてもらい、運動不足解消になってます。Lessonを習慣化し、日常生活でも自分の身体を意識することが出来てます。<br>
+           細く長く続けたいです。<br>
+           ありがとうございます。<br></p>
+        </div>
+
+        <div class="kuchikomi_card">
+           <figure class="kao_img"><img src="./img/main/A.jpg" alt="お客様6"></figure>
+           <h3 class="kuchikomi_name">Aさん<br>
+           女子ダイエット部受講</h3>
+           <p class="kuchikomi_text">食生活のアドバイスをもらい意識がかわりました。自分が食べたものを記録して食べ過ぎが抑えられています。運動習慣も大切ですが、食事も大切ですね。<br>
+           検診で引っ掛かってしまい、今までの生活を見直していきたいです。</p>
+        </div>
+
+       </section>
+     </div>
+    </div>
+
+
+
+    <!----------------------Q&A----------------------->
+    <section class="container5">
+      <div class="width1000px">
+      <div class="circle2">
+        <p class="circle2_text">Q&A</p>
+      </div>
+
+      <div class="qa_box">
+        <dl class="qa_textbox">
+          <dt class="Q_text"> 運動が苦手で体も固いのですが、初めてでもヨガは出来ますか？</dt>
+          <dd class="A_text">もちろんです!柔軟性や筋力は一人一人違います。バランスの良いポーズを取ることはヨガの練習になり、自分にとって良い体作りの基礎となります。スタジオレッスンとは別に毎週日曜朝のオンラインヨガの参加も可能です。オンラインヨガご希望方はZOOMでの参加となりますが、段取りもお伝えさせて頂きますので安心してご参加頂けます。</dd>
+        </dl>
+
+        <dl class="qa_textbox">
+          <dt class="Q_text"> 料金体型と支払い方法が知りたいです。 </dt>
+          <dd class="A_text">体験レッスン内容の3つの種類がそれぞれワンコイン(500円)で体験受講可能です。スタジオレッスンの場合はレッスン前に現金でお支払い可能です。オンラインレッスンの場合はクレジットカード Visa・MasterCard・AmericanExpressでのお支払いが可能です。また、キャンセルはレッスン1時間前まで可能ですので必ずご連絡をお願いしております。体験後のレッスンクラスの料金は「上記の体験レッスン内容」からコースの中身をご覧下さい。
+          </dd>
+        </dl>
+
+        <dl class="qa_textbox">
+          <dt class="Q_text"> レッスンのスケジュールが知りたいです。 </dt>
+          <dd class="A_text">オンラインヨガは毎週日曜 6:30〜開催しております。
+            スタジオでのグループレッスンは毎月日曜日と火曜日の不定期開催となっております。必ず事前にHPのトップ画面で日程を掲載しておりますのでご確認下さい。また、個人の為のパーソナルレッスンはご希望の日程の中から可能な日をお伝えさせて頂きます。</dd>
+        </dl>
+
+        <dl class="qa_textbox">
+          <dt class="Q_text"> 何を着ていけば良いですか？ </dt>
+          <dd class="A_text">基本的に動きやすい服装であれば大丈夫です。更衣スペースもございます。ウォーキングレッスンではある程度体のラインが分かる方がビフォーアフターを確認し易くなります。</dd>
+        </dl>
+      </div>
+    </div>
+    </section>
+ 
+
+    <!----------------お問い合わせフォーム----------------->
+    <div class="container6">
+      <section class="topmail_form" id="reserve&contact">
+          <h2 class="top-title">
+            <img src="./img/main/Reserve&Contact.png" alt="Reserve&Contact">
+          </h2>
+
+          <h3 class="R_Q">予約<br>&<br>お問い合わせ</h3>
+
+          <p class="attention">ご質問およびご相談は以下のメールフォームより<br>お気軽にご連絡ください。<br>
+           担当者より折り返し連絡いたします。
+          </p>
+
+
+      <!-----------入力スペース------------>
+      <form action="contact.php"  method="POST" class="mail-form" novalidate>
+        <dl class="mail-form_all">
+          <dt><span class="dt-title">必須</span><label for="inputname">お名前</label></dt>
+          <dd><input type="text" name="name" id="inputname" value="<?php echo htmlspecialchars($POST['name']) ?>" required autofocus></dd>
+
+          <?php if ($error['name']==='blank') :?>
+          <p>*名前をご記入下さい</p>
+          <?php endif; ?>
+
+
+
+          <dt><span class="dt-title">必須</span><label for="inputnumber">電話番号</label></dt>
+          <dd><input type="text" name="phone" id="inputnumber" value="<?php echo htmlspecialchars($POST['phone']) ?>" required autofocus></dd>
+
+          <?php if ($error['phone']==='phone'):?>
+            <p class="error_msg_p">*電話番号を正しくご記入下さい</P>
+          <?php endif;?>
+
+          <dt><span class="dt-title">必須</span><label for="inputaddress">Email</label></dt>
+          <dd><input type="text" name="e-mail" id="inputaddress"></dd>
+
+          <?php if ($error['email']==='email'):?>
+            <p class="error_msg">*メールアドレスを正しくご記入下さい</P>
+          <?php endif; ?>
+
+          <dt><label for="inputtext">自由記入欄（希望日時・体験レッスン希望内容等）</label></dt>
+          <dd><textarea class="textspace" name="text" id="inputtext" cols="50" rows="20" required><?php echo htmlspecialchars($POST['text']) ?></textarea></dd>
+
+          <?php if ($error['text']==='blank'): ?>
+            <p>*内容をご記入ください</p>
+          <?php endif;?>
+
+        </dl>
+        
+
+        <p class="policy">プライバシーポリシーに同意のうえ、<br>
+          送信してください</p>
+
+          <input type="image" name="submit" src="./img/main/soushin_button.png" onmouseover="this.src='./img/main/soushin_button_push.png'" onmouseout="this.src='./img/main/soushin_button.png'" alt="送信する" class="soushin_button" >
+        </form>
+      </section>
+    </div>
+    <!----------------------地図----------------------->
+    <div class="container7">
+      <h2 class="map">
+        <img src="./img/main/Access.png" alt="アクセス" class="access">
+
+        <hr class="border">
+        <p class="address">VAJA均整動術スタジオ<br>横浜市南区井土ヶ谷下町10-6</p>
+        <a class="MAP_button" href="https://www.google.com/maps/place/%E3%80%92232-0053+%E7%A5%9E%E5%A5%88%E5%B7%9D%E7%9C%8C%E6%A8%AA%E6%B5%9C%E5%B8%82%E5%8D%97%E5%8C%BA%E4%BA%95%E5%9C%9F%E3%82%B1%E8%B0%B7%E4%B8%8B%E7%94%BA%EF%BC%91%EF%BC%90%E2%88%92%EF%BC%96/@35.4314901,139.604689,17.32z/data=!4m5!3m4!1s0x60185b7c432f45b7:0xea8308c170a11be3!8m2!3d35.4316118!4d139.6058777" 
+        alt="地図を見る">詳しい地図を開く</a>
+
+      </h2>
+ 
+
+    </div>
+
+    <footer class="container8">
+      <footer_lp>
+        <p class="all_rights_r">2022 Yokohama Plus All Rights Reserved. </p>
+      </footer_lp>
+    </footer>
+
+    <!------レスポンシブ jQuery-responsive------->
+    <script>
+      document.getElementById("hamburger").addEventListener("click", function () {
+        this.classList.toggle("active");
+        document.getElementById("nav").classList.toggle("active");
+      })
+    </script>
+  
+</body>
+</html>
